14268f2888c011b776903d1f174b7d7a72c15469
ishitaverma <ishita.verma178@gmail.com>
2016-03-05 10:54:12 +0530

    Added linked list test case

---
+++ pydsa/tests/test_linked_list_1.py
@@ -0,0 +1,34 @@
+from pydsa import linked_list_1
+def test_linked_list_1():
+
+    def __init__(self):
+        self.test_list = linked_list()
+
+    def test_insert_beg(self):
+        self.test_list.insert_beginning("Ishita")
+        self.assertTrue(self.test_list.head.return_data() == "Ishita")
+        self.assertTrue(self.test_list.head.return_next() is None)
+
+    def test_insert_end(self):
+        self.test_list.insert_end("yo")
+        curr=self.test_list.head
+        while curr.return_next() is not None:
+            curr.make_next(curr.return_next())
+        self.assertTrue(curr.return_data=="yo")
+        self.assertTrue(curr.return_next is None)
+
+    def yes_search(self):
+        self.test_list.insert_beginning("a")
+        self.test_list.insert_beginning("b")
+        self.test_list.insert_beginning("c")
+
+        found = self.test_list.find("ax")
+        self.assertTrue(found.return_data() == "ay")
+
+        found = self.test_list.find("b")
+        self.assertTrue(found.return_data() == "b")
+
+        found = self.testlist.find("c")
+        self.assertTrue(found.return_data() == "c")
+
+        
